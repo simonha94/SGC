db36222c7662b3abf8c68cec3fbd2646b93776e4
simonha94 <58327915+simonha94@users.noreply.github.com>
2021-07-21 09:22:16 +0200

    Update normalization.py
    implemented other adjacency normalization methods

--- normalization.py
+++ normalization.py
@@ -12,19 +12,37 @@ def aug_normalized_adjacency(adj):
    return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
 
 def normalized_adjacency(adj):
-   pass
+   """graph project, sh 2021"""
+   row_sum = np.array(adj.sum(1))
+   d_inv_sqrt = np.power(row_sum, -0.5).flatten()
+   d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+   d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+   return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
 
 def random_walk_adjacency(adj):
-   pass
+   """graph project, sh 2021"""
+   row_sum = np.array(adj.sum(1))
+   d_inv_sqrt = np.power(row_sum, -1).flatten()
+   d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+   return d_mat_inv_sqrt.dot(adj).tocoo()
 
 def aug_random_walk_adjacency(adj):
-   pass
+   """graph project, sh 2021"""
+   adj = adj + sp.eye(adj.shape[0])
+   adj = sp.coo_matrix(adj)
+   row_sum = np.array(adj.sum(1))
+   d_inv_sqrt = np.power(row_sum, -1).flatten()
+   d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+   d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+   return d_mat_inv_sqrt.dot(adj).tocoo()
 
 def first_order_cheby_adjacency(adj):
-   pass
+   """graph project, sh 2021"""
+   adj = normalized_adjacency(adj)
+   return sp.eye(adj.shape[0]) + adj
 
 def fetch_normalization(type):
-   switcher = {
+   switcher = {                                 # It is not distinguished between "hat" and "not hat" in the comment formulars!!!
        'AugNormAdj': aug_normalized_adjacency,  # A' = (D + I)^-1/2 * ( A + I ) * (D + I)^-1/2
        'AugRandAdj': aug_random_walk_adjacency, # A' = (D + I)^-1 * (A + I)
        'NormAdj': normalized_adjacency,         # A' =  D^-1/2 * A * D^-1/2
